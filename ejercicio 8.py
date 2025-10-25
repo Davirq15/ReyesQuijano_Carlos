2ff6a1a79f2adb86bee12ff06242ff394cfffb16
Davirq15 <alu.2611557@itesrenedescartes.edu.mx>
2025-10-24 20:18:52 -0600

    Delete ejercicio 8.py

--- ejercicio 8.py
+++
@@ -1,8 +0,0 @@
-#Haz un programa en Python que pida tres números y muestre si los tres son iguales (solo mostrando True o False).
-
-num1 = float(input("Introduce el primer número: "))
-num2 = float(input("Introduce el segundo número: "))
-num3 = float(input("Introduce el tercer número: "))
-
-resultado = (num1 == num2 == num3)
-print(f"¿Los tres números son iguales? {resultado}")

3066e9fac7a5f23a5c111eaf139cd150d7a18eb7
Davirq15 <alu.2611557@itesrenedescartes.edu.mx>
2025-10-24 20:12:48 -0600

    Delete ejercicio 1.py

--- ejercicio 1.py
+++
@@ -1,9 +0,0 @@
-#Haz un programa en Python que pida tres calificaciones y calcule su promedio con dos decimales.
-#(Investiga la función round() en Python).
-
-cal1 = float(input("Introduce la primera calificación: "))
-cal2 = float(input("Introduce la segunda calificación: "))
-cal3 = float(input("Introduce la tercera calificación: "))
-
-promedio = round((cal1 + cal2 + cal3) / 3, 2)   #la función round() redondea el resultado a 2 decimales
-print(f"El promedio de las calificaciones es: {promedio}")

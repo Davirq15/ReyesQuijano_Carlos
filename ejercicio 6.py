4bb2844aa9ff46cd3fe02525ce81dc06a4cc2d09
Davirq15 <alu.2611557@itesrenedescartes.edu.mx>
2025-10-24 20:17:40 -0600

    Delete ejercicio 6.py

--- ejercicio 6.py
+++
@@ -1,8 +0,0 @@
-#Haz un programa en Python que pida tres números y muestre si se cumple la expresión A < B < C (solo mostrando el resultado lógico).
-
-num1 = float(input("Introduce el primer número: "))
-num2 = float(input("Introduce el segundo número: "))
-num3 = float(input("Introduce el tercer número: "))
-
-resultadoLogico = num1 < num2 < num3
-print(f"¿Se cumple la expresión A < B < C? {resultadoLogico}")
